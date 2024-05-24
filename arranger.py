75dc24c339f627ab2e301cc83b44b2b11fdddc05
Gwillyn Donaghy <gwillynd@gmail.com>
2024-05-24 17:13:44 -0400

    Update arranger.py

--- arranger.py
+++ arranger.py
@@ -2,7 +2,7 @@ def arithmetic_arranger(problems, show_answers=False):
     
     # Error: if amount of problems given is greater than 5
     if len(problems) > 5: 
-        return print('Error: Too many problems.')
+        return 'Error: Too many problems.'
    
     # separate equation into operators
     operators = []
@@ -11,7 +11,7 @@ def arithmetic_arranger(problems, show_answers=False):
         operators.extend([p[1]])
     # Error: check if operators match only '+' and '-'
     if not all(map(lambda x: x == '+' or x=='-', operators)):
-        return print("Error: Operator must be '+' or '-'.")
+        return "Error: Operator must be '+' or '-'."
     # Error: separate equation into parts of individuals
     numbers = []
     for i in problems: 
@@ -19,14 +19,27 @@ def arithmetic_arranger(problems, show_answers=False):
         numbers.extend([p[0], p[2]])
     # Error: check if numbers are digits 
     if not all(map(lambda x: x.isdigit(), numbers)):
-        return print('Error: Numbers must only contain digits.')
+        return 'Error: Numbers must only contain digits.'
     # Error: check if numbers are greater than 4
     if not all(map(lambda x: len(x) < 5, numbers)):
-            return print('Error: Numbers cannot be more than four digits.')
+            return 'Error: Numbers cannot be more than four digits.'
 
 
+    # equates the solutions to the problems and saves them in a list.
+    solutions = []
+    for i in problems:
+        p = i.split()
+        if p[1] == '+':
+            solution = int(p[0]) + int(p[2])
+            solutions.append(solution)
+        elif p[1] == '-':
+            solution = int(p[0]) - int(p[2])
+            solutions.append(solution)
+        
+    
 
-    return problems
+
+    return problems, solutions 
 
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+print(f'\n{arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"])}')
